7be6d73f98c24a98583475c4b85e33c754e7bfb0
Petr Gajdos <pgajdos@suse.cz>
2014-12-04 00:16:23 +0100

    show script coverages in add family dialog

--- src/lib/fonts/fpl-add-dialog.rb
+++ src/lib/fonts/fpl-add-dialog.rb
@@ -1,4 +1,5 @@
 require "yast/fontconfig_setting"
+require "yast/font_specimen"
 
 module FontsConfig
   class FPLAddDialog
@@ -6,6 +7,7 @@ module FontsConfig
     include UIShortcuts
     include I18n
     include FontconfigSetting
+    include FontSpecimen
 
     BLACKLIST = [
       "micro.pcf",
@@ -19,12 +21,19 @@ module FontsConfig
       @available_families = installed_families(["family", "fontformat"])
       # delete families, that are part of list for some alias
       if (@available_families)
-         @fcstate.fpl.keys.each do |key|
+        @fcstate.fpl.keys.each do |key|
           @fcstate.fpl[key].each do |family|
             if (@available_families.index(family))
               @available_families.delete_if{|f| f =~ /#{family}/}
             end
           end
+        end
+        @available_families.each do |family|
+          pattern = parse_pattern(family)
+          scripts = font_scripts(pattern["family"]).
+                      map{|script, coverage| "#{script}(#{coverage})"}.
+                        join(',')
+          family << ':scripts=' << scripts
         end
       end
     end
@@ -33,18 +42,20 @@ module FontsConfig
       Yast.import "UI"
       items = @available_families.map do |family|
                 pattern = parse_pattern(family)
-                Item(Id(pattern["family"]), pattern["family"], pattern["fontformat"])
+                Item(Id(pattern["family"]), pattern["family"], 
+                        pattern["fontformat"], pattern["scripts"])
               end
 
       dialog_content = VBox(
                         InputField(Id("txt_family_name"), 
                                    Opt(:notify, :immediate, :hstretch),
                                    _("&Filter"), ""),
-                        MinSize(40, 16,
+                        MinSize(70, 16,
                                 Table(Id("tbl_family_names"),
                                       Opt(:notify, :immediate),
                                       Header(_("Installed Families"), 
-                                             _("Font Format")),
+                                             _("Font Format"),
+                                             _("Script Coverages")),
                                       items)),
                         HBox(PushButton(Id("btn_cancel"), _("&Cancel")),
                              PushButton(Id("btn_add"), _("&Add"))),
@@ -63,6 +74,7 @@ module FontsConfig
       pattern = Hash.new
       pattern["family"] = str_pattern[/^[^:]*/]
       pattern["fontformat"] = str_pattern.gsub(/.*fontformat=([^:]+).*/, '\1')
+      pattern["scripts"] = str_pattern.gsub(/.*scripts=([^:]+).*/, '\1')
       pattern
     end
 
@@ -78,7 +90,8 @@ module FontsConfig
             filtered_families = @available_families.select{|f| f[/#{substring}/i]}
             items = filtered_families.map do |family|
                     pattern = parse_pattern(family)
-                    Item(Id(pattern["family"]), pattern["family"], pattern["fontformat"])
+                    Item(Id(pattern["family"]), pattern["family"], 
+                         pattern["fontformat"], pattern["scripts"])
                     end
             UI.ChangeWidget(Id("tbl_family_names"),
                             :Items, items)

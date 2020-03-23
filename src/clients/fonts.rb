8d7f88053b3be6975ee3fc0421c1eb38dbc551e4
Ladislav Slezák <lslezak@suse.cz>
2020-03-23 12:23:05 +0100

    Remove all content

--- src/clients/fonts.rb
+++
@@ -1,6 +0,0 @@
-# encoding: utf-8
-#
-
-require "fonts/fonts-dialog"
-
-FontsConfig::FontsConfigDialog.run
--- test/spec-helper.rb
+++
@@ -1,5 +0,0 @@
-# encoding: utf-8
-#
-
-ENV["Y2DIR"] = File.expand_path("../../src", __FILE__)
-require "yast"
